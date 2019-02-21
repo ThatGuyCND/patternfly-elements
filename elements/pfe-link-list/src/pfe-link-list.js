d735219b7b387f745b026680cfd2cb0981357aa7
castastrophe <casnroberts+github@gmail.com>
2019-02-21 09:21:46 -0500

    Add schema plugins for the js file

--- elements/pfe-link-list/src/pfe-link-list.js
+++ elements/pfe-link-list/src/pfe-link-list.js
@@ -5,6 +5,10 @@ class PfeLinkList extends PFElement {
     return "pfe-link-list";
   }
 
+  get schemaUrl() {
+    return "pfe-link-list.json";
+  }
+
   get templateUrl() {
     return "pfe-link-list.html";
   }
@@ -17,17 +21,33 @@ class PfeLinkList extends PFElement {
   //   return [];
   // }
 
-  constructor() {
-    super(PfeLinkList);
+  // Declare the type of this component
+  static get PfeType() {
+    return PFElement.PfeTypes.Content;
   }
 
-  // connectedCallback() {
-  //   super.connectedCallback();
-  // }
+  constructor() {
+    super(PfeLinkList, { type: PfeLinkList.PfeType });
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+  }
 
   // disconnectedCallback() {}
 
-  // attributeChangedCallback(attr, oldValue, newValue) {}
+  attributeChangedCallback(attr, oldValue, newValue) {
+    super.attributeChangedCallback(attr, oldValue, newValue);
+    // Strip the prefix from the attribute
+    attr = attr.replace("pfe-", "");
+    // If the observer is defined in the attribute properties
+    if (this[attr] && this[attr].observer) {
+      // Get the observer function
+      let observer = this[this[attr].observer].bind(this);
+      // If it's a function, allow it to run
+      if (typeof observer === "function") observer(attr, oldValue, newValue);
+    }
+  }
 }
 
 PFElement.create(PfeLinkList);
